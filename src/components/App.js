9609dcb3b86fefd5a8407babbdbea4de5f63f678
Zeon <zeonstewart23@gmail.com>
2021-09-07 19:15:37 -0400

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,37 @@
 // create your App component here
+import React, {useState, useEffect} from 'react'
+
+
+
+function App(){
+    const [status, setStatus ] = useState('pending');
+    const [pics, setPics] = useState([])
+    useEffect(()=>{fetch('https://dog.ceo/api/breeds/image/random')
+                    .then(resp => resp.json())
+                    .then(data=> {
+                        setPics(data.message)
+                        setStatus('success')
+                    });}, []);
+    
+    if (status === 'pending'){
+        return <h1>Loading...</h1>
+    }
+
+
+
+
+
+
+
+
+
+
+    return(
+        <>
+        <img src={pics} alt={'A Random Dog'}/>
+        </>
+    )
+}
+
+
+export default App;
